81939eda7fdebab1e6de6fca729829d45aba815a
subbotaevgenij <gogi_2_nogi@mail.ru>
2026-04-03 09:47:11 +0300

    Second commit

---
+++ clicker_project/clicker_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, ProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+]
---
+++ clicker_project/clicker_app/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, ProfileSerializer
+from .models import Profile
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        profile = request.user.profile
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def post(self, request):
+        profile = request.user.profile
+        profile.clicks += 1
+        profile.save()
+        return Response({'clicks': profile.clicks})
