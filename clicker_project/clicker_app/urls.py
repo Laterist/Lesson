a69c94642f2dca751efcd49cac4837b3f4610bcd
Laterist <romakozlov2010@gmail.com>
2026-05-02 18:56:20 +0300

    Update urls.py

--- clicker_project/clicker_app/urls.py
+++ clicker_project/clicker_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/reset/', ProfileView.as_view(), name='profile_reset')
 ]
